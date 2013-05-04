347a25c9eff0e745db924cf124a0f6d218ce0ba3
manufran78 <manufran@me.com>
2013-05-04 13:26:24 -0700

    Juego de dados ultma version

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -6,3 +6,19 @@ a = rand(6) + 1
 puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
 b = rand(6) + 1
 puts "=> la computadora hizo #{b}!"
+
+if a > b
+
+ puts "=> Hey #{nombre} ganaste! Tuviste suerte!"
+
+elsif a < b
+
+ puts "=> Hey #{nombre} eres un loser! Yo gane!"
+
+else
+
+ a == b
+
+puts "=> Empate! Amigos como antes!"
+
+end
