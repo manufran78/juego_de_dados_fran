3d47b7f8aad84843da028f1f738a700c599c0a76
manufran78 <manufran@me.com>
2013-05-04 15:14:59 -0700

    Dos Dados

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -1,8 +1,20 @@
-puts "=> Bienvenido al Juego De Dados"
-puts "=>Hola, como te llamas?"
+auts "=>Hola, como te llamas?"
 nombre = gets.chomp
-puts "=> Hola " + nombre + " vamos a jugar?"
-a = rand(6) + 1
+puts "=> Hola " + nombre + " vamos a jugar con dos dados!"
+
+dado_1 = rand (6) +1
+dad0_2 = rand (6) +1 
+total = dado_1 + dado_2
+
+resultado_jugador = { dado_1 . dado_2, total}
+
+# el jugador lanza su propio dado
+
+puts "=> Hola #{nombre} hiciste #{resultado_jugador[0]} con tu dado!"
+
+= begin
+# la computadora lanza su propio dado
+b = rand(6) + 1  
 puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
 b = rand(6) + 1
 puts "=> la computadora hizo #{b}!"
